a9b20ddff2dd87a8f04eeb22c328f45e7ca45d0d
John Ledbetter <john.ledbetter@gmail.com>
2023-06-16 12:13:42 -0400

    modularize

---
+++ src/screen.rs
@@ -0,0 +1,58 @@
+pub struct Screen {
+  display: [[u8; Screen::WIDTH]; Screen::HEIGHT],
+}
+
+pub enum ScreenUpdate {
+  Draw {
+    bytes: Vec<u8>,
+    coords: (usize, usize),
+  },
+  Clear,
+}
+
+impl Screen {
+  const WIDTH: usize = 64;
+  const HEIGHT: usize = 32;
+
+  pub fn new() -> Self {
+    Self {
+      display: [[0; Screen::WIDTH]; Screen::HEIGHT],
+    }
+  }
+
+  pub fn update(&mut self, msg: &ScreenUpdate) {
+    match msg {
+      ScreenUpdate::Draw { coords, bytes } => {
+        let (x, mut y) = coords;
+        for byte in bytes {
+          for i in 0..8 {
+            self.display[y][x + i] ^= if byte & 1 << ((7 - i) as u8) != 0 {
+              1
+            } else {
+              0
+            };
+          }
+          y = (y + 1) % 64;
+        }
+      }
+      ScreenUpdate::Clear => {
+        self.display = [[0; Screen::WIDTH]; Screen::HEIGHT];
+      }
+    }
+  }
+
+  pub fn draw(&self, frame: &mut [u8]) {
+    for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
+      let x = (i % 64) as usize;
+      let y = (i / 64) as usize;
+
+      let color = if self.display[y][x] != 0 {
+        [0xFF, 0xFF, 0xFF, 0xFF]
+      } else {
+        [0x00, 0x00, 0x00, 0xFF]
+      };
+
+      pixel.copy_from_slice(&color);
+    }
+  }
+}
